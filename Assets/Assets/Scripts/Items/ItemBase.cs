13897936b8b89a5eac2c399edcfff0cbc0263cc5
s29788 <s29788@pjwstk.edu.pl>
2026-03-11 15:19:36 +0100

    .

---
+++ Assets/Assets/Scripts/Items/ItemBase.cs
@@ -0,0 +1,24 @@
+﻿using Player;
+using UnityEngine;
+
+namespace Items
+{
+    public class ItemBase : MonoBehaviour, IInteractable
+    {
+        [SerializeField] private PlayerController player;
+        void Update()
+        {
+            float distance = Vector3.Distance(player.transform.position, transform.position);
+
+            if (distance < 1f)
+            {
+                Interact();
+            }
+        }
+
+        public void Interact()
+        {
+            
+        }
+    }
+}
