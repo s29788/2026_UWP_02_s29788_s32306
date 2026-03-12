cb0cb4fa2250734584dee91b5e070ee2f9f47b5b
Olaf <s32306@pjwstk.edu.pl>
2026-03-12 20:11:41 +0100

    quick fix: collider to collider2D

--- Assets/Assets/Scripts/Items/ItemCollector.cs
+++ Assets/Assets/Scripts/Items/ItemCollector.cs
@@ -11,7 +11,7 @@ namespace Items
             _playerReferences = GetComponent<PlayerReferences>();
         }
 
-        private void OnTriggerEnter(Collider collision)
+        private void OnTriggerEnter2D(Collider2D collision)
         {
             ItemBase item = collision.GetComponent<ItemBase>();
             if (item != null)
